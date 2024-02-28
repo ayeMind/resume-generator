5ec912cd0d0eebb891ccda7a6c1b52e9dc666829
Yan <fzerix@gmail.com>
2024-02-28 14:08:59 +0300

    all js

--- solution/scripts/index.js
+++ solution/scripts/index.js
@@ -1,4 +1,4 @@
-function createPersonalInfoComponent() {
+function createPersonalInfoComponent(personalInfoItems) {
     const personalInfoComponent = document.createElement('div');
     personalInfoComponent.className = 'personal-info';
 
@@ -10,13 +10,8 @@ function createPersonalInfoComponent() {
     personalInfoHr.className = 'side-hr';
     personalInfoComponent.appendChild(personalInfoHr);
 
-    const personalInfoItems = [
-        { title: 'ФИО', content: 'Данилов Дмитрий Евгеньевич' },
-        { title: 'Дата рождения', content: '05.05.1986' },
-        { title: 'Город', content: 'Ижевск' },
-        { title: 'Номер телефона', content: '+7 (234) 228-18-15' },
-        { title: 'Email', content: 'beautifulfly@yandex.ru' }
-    ];
+    const container = document.createElement('div');
+    container.className = 'personal-container';
 
     personalInfoItems.forEach(item => {
         const infoItem = document.createElement('div');
@@ -31,13 +26,14 @@ function createPersonalInfoComponent() {
         infoItem.appendChild(title);
         infoItem.appendChild(content);
 
-        personalInfoComponent.appendChild(infoItem);
+        container.appendChild(infoItem);
     });
 
+    personalInfoComponent.appendChild(container);
     return personalInfoComponent;
 }
 
-function createInterestsComponent() {
+function createInterestsComponent(interests) {
     const interestsComponent = document.createElement('div');
     interestsComponent.className = 'personal-info';
 
@@ -48,8 +44,6 @@ function createInterestsComponent() {
     const interestsHr = document.createElement('hr');
     interestsHr.className = 'side-hr';
     interestsComponent.appendChild(interestsHr);
-
-    const interests = ['Хороший лидер', 'Занимаюсь спортом', 'Строительные практики', 'Выпускал журналы'];
 
     interests.forEach(interest => {
         const interestItem = document.createElement('div');
@@ -66,8 +60,7 @@ function createInterestsComponent() {
 }
 
 
-// В шаблоне здесь сдвигается все вниз, вот и я этот сдвиг сделал, ничего не знаю
-function createLanguagesComponent() {
+function createLanguagesComponent(languages) {
     const languagesComponent = document.createElement('div');
     languagesComponent.className = 'personal-info lang-container';
 
@@ -78,11 +71,6 @@ function createLanguagesComponent() {
     const languagesHr = document.createElement('hr');
     languagesHr.className = 'side-hr';
     languagesComponent.appendChild(languagesHr);
-
-    const languages = [
-        { language: 'Английский', level: 'C1' },
-        { language: 'Испанский', level: 'B2' }
-    ];
 
     languages.forEach(lang => {
         const langItem = document.createElement('div');
@@ -103,68 +91,74 @@ function createLanguagesComponent() {
     return languagesComponent;
 }
 
-function createMainInfoComponent() {
+function createMainInfoComponent(mainInfo, titleText) {
     const mainInfoComponent = document.createElement('div');
     mainInfoComponent.className = 'main-info';
 
     const mainInfoTitle = document.createElement('h2');
-    mainInfoTitle.textContent = 'Опыт работы';
+    mainInfoTitle.textContent = titleText;
     mainInfoComponent.appendChild(mainInfoTitle);
 
     const mainInfoHr = document.createElement('hr');
     mainInfoHr.className = 'main-info-hr';
     mainInfoComponent.appendChild(mainInfoHr);
 
-    const jobExperiences = [
-        {
-            title: 'C++ разработчик',
-            company: 'ООО Рога и Копыта, Москва',
-            date: 'сентябрь 2020 г. — наст. время',
-            description: 'Писал компилятор под js, который позволял ускорить билд приложений.'
-        },
-        {
-            title: 'Angular-разработчик',
-            company: 'Тинькофф Центр Разработки, Ижевск',
-            date: 'февраль 2020 г. — август 2020 г.',
-            description: 'Придумывал аналитические решения и разрабатывал веб-сайты для улучшения опыта пользователей.'
-        }
-    ];
+    const mainList = document.createElement('div');
+    if (!titleText.includes('Курсы')) {
+        mainList.className = 'main-info-list';
+    } else {
+        mainList.className = 'main-info-list no-description';
+    }
 
-    jobExperiences.forEach(experience => {
-        const experienceItem = document.createElement('div');
-        experienceItem.className = 'main-info-item';
+    mainInfo.forEach(item => {
+
+        const itemDiv = document.createElement('div');
+        itemDiv.className = 'main-info-item';
+
+        const mainInfoRow = document.createElement('div');
+        mainInfoRow.className = 'main-info-row';
+
+        const mainInfoColumn = document.createElement('div');
+        mainInfoColumn.className = 'main-info-column';
 
         const title = document.createElement('h3');
-        title.textContent = experience.title;
+        title.textContent = item.title;
 
-        const company = document.createElement('p');
-        company.textContent = experience.company;
+        const where = document.createElement('h4');
+        where.textContent = item.where;
+
+        mainInfoColumn.appendChild(title);
+        mainInfoColumn.appendChild(where);
 
         const date = document.createElement('div');
         date.className = 'date';
-        date.innerHTML = `<p>${experience.date}</p>`;
+        date.innerHTML = `<p>${item.date}</p>`;
 
-        const description = document.createElement('div');
-        description.className = 'main-info-description';
-        description.innerHTML = `<p>${experience.description}</p>`;
+        mainInfoRow.appendChild(mainInfoColumn);
+        mainInfoRow.appendChild(date);
 
-        experienceItem.appendChild(title);
-        experienceItem.appendChild(company);
-        experienceItem.appendChild(date);
-        experienceItem.appendChild(description);
-
-        mainInfoComponent.appendChild(experienceItem);
+        itemDiv.appendChild(mainInfoRow);
+        if (item.description) {
+            const description = document.createElement('div');
+            description.className = 'main-info-description';
+            description.innerHTML = `<p>${item.description}</p>`;
+            itemDiv.appendChild(description);
+        }
+       
+        mainList.appendChild(itemDiv);
     });
+
+    mainInfoComponent.appendChild(mainList);
 
     return mainInfoComponent;
 }
 
-function createTitleComponent() {
+function createTitleComponent(titleText) {
     const titleComponent = document.createElement('div');
     titleComponent.className = 'title';
 
     const title = document.createElement('h1');
-    title.textContent = 'Данилов Дмитрий Евгеньевич';
+    title.textContent = titleText;
 
     titleComponent.appendChild(title);
 
@@ -175,30 +169,40 @@ function createTitleComponent() {
     return titleComponent;
 }
 
-function createDescriptionComponent() {
+function createDescriptionComponent(descriptionText) {
     const descriptionComponent = document.createElement('div');
     descriptionComponent.className = 'description';
 
-    const descriptionText = document.createElement('p');
-    descriptionText.textContent = 'В целом достаточно сильный разработчик, я бы даже сказал умный, вообще умен не по годам. Подниму ваш проект, удалю все легаси, и все коммиты будут маленькими по 15 строк кода.';
+    const descriptionTextElement = document.createElement('p');
+    descriptionTextElement.className = 'long';
 
-    descriptionComponent.appendChild(descriptionText);
+    descriptionTextElement.textContent = descriptionText;
+
+    descriptionComponent.appendChild(descriptionTextElement);
 
     return descriptionComponent;
 }
 
-function createMainContainer() {
+function createMainContainer(titleText, descriptionText, job, education, courses) {
     const mainContainer = document.createElement('div');
     mainContainer.className = 'main-container';
 
-    mainContainer.appendChild(createTitleComponent());
-    mainContainer.appendChild(createDescriptionComponent());
-    mainContainer.appendChild(createMainInfoComponent());
+    mainContainer.appendChild(createTitleComponent(titleText));
+    mainContainer.appendChild(createDescriptionComponent(descriptionText));
+
+   const mainInfoContainer = document.createElement('div');
+    mainInfoContainer.className = 'main-info-container';
+
+    mainInfoContainer.appendChild(createMainInfoComponent(job, 'Опыт работы'));
+    mainInfoContainer.appendChild(createMainInfoComponent(education, 'Образование и квалификация'));
+    mainInfoContainer.appendChild(createMainInfoComponent(courses, 'Курсы'));
+
+    mainContainer.appendChild(mainInfoContainer);
 
     return mainContainer;
 }
 
-function createSidePanel() {
+function createSidePanel(imageSrc, imageAlt, personalInfoItems, interests, languages) {
     const sidePanel = document.createElement('div');
     sidePanel.className = 'side-panel';
 
@@ -206,8 +210,8 @@ function createSidePanel() {
     imageContainer.className = 'image-container';
 
     const image = document.createElement('img');
-    image.src = 'images/photo.jpg';
-    image.alt = 'Фото';
+    image.src = imageSrc;
+    image.alt = imageAlt;
     image.className = 'image';
 
     imageContainer.appendChild(image);
@@ -216,21 +220,21 @@ function createSidePanel() {
     const personalContainer = document.createElement('div');
     personalContainer.className = 'personal';
 
-    personalContainer.appendChild(createPersonalInfoComponent());
-    personalContainer.appendChild(createInterestsComponent());
-    personalContainer.appendChild(createLanguagesComponent());
+    personalContainer.appendChild(createPersonalInfoComponent(personalInfoItems));
+    personalContainer.appendChild(createInterestsComponent(interests));
+    personalContainer.appendChild(createLanguagesComponent(languages));
 
     sidePanel.appendChild(personalContainer);
 
     return sidePanel;
 }
 
-function createResumePageComponent() {
+function createResumePageComponent(imageSrc, imageAlt, titleText, descriptionText, personalInfoItems, interests, languages, job, education, courses) {
     const resumePage = document.createElement('div');
     resumePage.className = 'container';
 
-    const sidePanel = createSidePanel();
-    const mainContainer = createMainContainer();
+    const sidePanel = createSidePanel(imageSrc, imageAlt, personalInfoItems, interests, languages);
+    const mainContainer = createMainContainer(titleText, descriptionText, job, education, courses);
 
     resumePage.appendChild(sidePanel);
     resumePage.appendChild(mainContainer);
@@ -238,4 +242,74 @@ function createResumePageComponent() {
     return resumePage;
 }
 
-document.body.appendChild(createResumePageComponent());
+const personalInfoItems = [
+    { title: 'ФИО', content: 'Данилов Дмитрий Евгеньевич' },
+    { title: 'Дата рождения', content: '05.05.1986' },
+    { title: 'Город', content: 'Ижевск' },
+    { title: 'Номер телефона', content: '+7 (234) 228-18-15' },
+    { title: 'Email', content: 'beautifulfly@yandex.ru' }
+];
+
+const interests = ['Хороший лидер', 'Занимаюсь спортом', 'Строительные практики', 'Выпускал журналы'];
+
+const languages = [
+    { language: 'Английский', level: 'C1' },
+    { language: 'Испанский', level: 'B2' },
+];
+
+const job = [
+    {
+        title: 'C++ разработчик',
+        where: 'ООО Рога и Копыта, Москва',
+        date: 'сентябрь 2020 г. — наст. время',
+        description: 'Писал компилятор под js, который позволял ускорить билд приложений.'
+    },
+    {
+        title: 'Angular-разработчик',
+        where: 'Тинькофф Центр Разработки, Ижевск',
+        date: 'февраль 2020 г. — август 2020 г.',
+        description: 'Придумывал аналитические решения и разрабатывал веб-сайты для улучшения опыта пользователей.'
+    }
+];
+
+const education = [
+    {
+        title: 'Магистратура',
+        where: 'ЦУ, Москва',
+        date: 'сентябрь 2020 г. — наст. время',
+        description: 'Дизайн и разработка ПО'
+    },
+    {
+        title: 'Бакалавриат',
+        where: 'Уральский Федеральный Университет, Екатеринбург',
+        date: 'февраль 2020 г. — август 2020 г.',
+        description: 'Направление: МОАИС.'
+    }
+];
+    
+const courses = [
+    {
+        title: 'Школа промышленной разработки',
+        where: 'Известная компания',
+        date: 'январь 2021 г. — май 2021 г.',
+    },
+    {
+        title: 'Основы JavaScript, HTML, CSS',
+        where: 'ЦУ',
+        date: 'февраль 2020 г. — август 2020 г.',
+    },
+    {
+        title: 'Разработка на C++',
+        where: 'Образование для Всех',
+        date: 'январь 2019 г. — январь 2020 г.',
+    },
+];
+
+
+
+const imageSrc = 'images/photo.jpg';
+const imageAlt = 'Фото';
+const titleText = 'Данилов Дмитрий Евгеньевич';
+const descriptionText = 'В целом достаточно сильный разработчик, я бы даже сказал умный, вообще умен не по годам. Подниму ваш проект, удалю все легаси, и все коммиты будут маленькими по 15 строк кода.';
+
+document.body.appendChild(createResumePageComponent(imageSrc, imageAlt, titleText, descriptionText, personalInfoItems, interests, languages, job, education, courses));
